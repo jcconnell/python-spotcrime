846a1263f24e50914a288ef36ad07af2d9e7ff83
JC Connell <jamescarltonconnell@gmail.com>
2018-02-13 14:28:57 -0500

    Working through tests

--- tests/test_spotcrime.py
+++ tests/test_spotcrime.py
@@ -12,13 +12,13 @@ class TestSpotCrime(unittest.TestCase):
 
     def test_incident_transform(self):
         incident = {
-            'incident_id': 1,
-            'parent_incident_type': 'type',
-            'timestamp': '2017-02-02T12:33:00.000',
-            'parent_incident_lat': '-73.995808',
-            'parent_incident_lon': '40.711806',
-            'location': 'ADDRESS',
-            'incident_link': 'https://spotcrime.com/',
+            'cdid': 1,
+            'type': 'type',
+            'date': '2017-02-02T12:33:00.000',
+            'lat': '-73.995808',
+            'lon': '40.711806',
+            'address': 'ADDRESS',
+            'link': 'https://spotcrime.com/',
         }
         self.assertEqual(spotcrime._incident_transform(incident), {
             'id': 1,
@@ -32,13 +32,13 @@ class TestSpotCrime(unittest.TestCase):
 
     def test_validate_incident_types(self):
         with self.assertRaises(ValueError):
-            crimereports._validate_incident_types(['Alarm', 'Bad Type'])
+            spotcrime._validate_incident_types(['Alarm', 'Bad Type'])
 
     def test_get_map_url(self):
-        sc = spotcrime.SpotCrime(NEWARK, 0.01)
-        self.assertTrue(sc.get_map_url().startswith('https://www.spotcrime.com'))
+        sc = spotcrime.SpotCrime(NEWARK, 0.01, None, None, 10)
+        self.assertTrue(sc.get_map_url().startswith('https://spotcrime.com'))
 
     def test_get_incidents(self):
-        sc = spotcrime.SpotCrime(NEWARK, 0.01)
+        sc = spotcrime.SpotCrime(NEWARK, 0.01, None, None, 10)
         incidents = sc.get_incidents()
         self.assertIsInstance(incidents, list)
