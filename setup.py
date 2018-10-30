4c7e714b77e8296ba943a02328ab8f75f50fa405
JC Connell <jamescarltonconnell@gmail.com>
2018-10-30 15:53:28 -0400

    Update requests dependency to ~> 2.20.0

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ from setuptools import find_packages, setup
 
 setup(
     name='spotcrime',
-    version='1.0.3',
+    version='1.0.4',
     description='Provides basic API to spotcrime.com.',
     url='https://github.com/jcconnell/python-spotcrime',
     license='MIT',
     author='jcconnell',
     author_email='jamescarltonconnell@gmail.com',
     packages=find_packages(),
-    install_requires=['requests==2.12.4'],
+    install_requires=['requests ~> 2.20.0'],
     classifiers=[
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
